312c0869e98443528039bdb94c18b02979964dd0
Jacob-Waugh <126210762+Jacob-Waugh@users.noreply.github.com>
2026-01-15 17:26:51 -0500

    Added site navigation

---
+++ _site/assets/js/NavMenu.js
@@ -0,0 +1,32 @@
+function openNav(){
+  console.log("Menu opened");
+  document.getElementById("sideNav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
+}
+
+function closeNav(){
+  console.log("Menu closed");
+  document.getElementById("sideNav").style.width = "0px";
+  document.body.style.backgroundColor = "white";
+  dropdownBox.style.display = "none";
+  dropdown[0].innerHTML = "Projects &#9662;";
+}
+
+//Dropdown menu:
+var dropdown = document.getElementsByClassName("dropdownBtn");
+var i;
+
+for (i = 0; i < dropdown.length; i++){
+  dropdown[i].addEventListener("click", function(){
+    this.classList.toggle("active");
+    var dropdownBox = this.nextElementSibling;
+    if(dropdownBox.style.display === "block"){
+      dropdownBox.style.display = "none";
+      dropdown[i].innerHTML = "Projects &#9662;";
+    }
+    else{
+      dropdownBox.style.display = "block";
+      dropdown[i].innerHTML = "Projects &#9652;";
+    }
+  })
+}
